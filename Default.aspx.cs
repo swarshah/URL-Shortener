940fbe2e93eb9a88e8e1ad9ba03ac0d3d645e6e8
SwarShah <swar12894@gmail.com>
2015-04-04 03:21:26 -0400

    encodeInt methods

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -7,18 +7,21 @@ using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data.Sql;
 using System.Configuration;
+using System.Text;
 
 public partial class _Default : System.Web.UI.Page
 {
-    String connectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\Swar\Documents\Visual Studio 2013\WebSites\UrlShortener\App_Data\Database.mdf';Integrated Security=True";
+    private String connectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='C:\Users\Swar\Documents\Visual Studio 2013\WebSites\UrlShortener\App_Data\Database.mdf';Integrated Security=True";
+    private String ALPHABET = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
-        int id = getid();
-        shortLink.Text = id.ToString();
+        //int id = getid();
+        //shortLink.Text = id.ToString();
+        shortLink.Text = encodeInt(getid());
     }
 
     protected int getid()
@@ -31,5 +34,16 @@ public partial class _Default : System.Web.UI.Page
             cn.Close();
             return id+1;
         }
+    }
+
+    protected String encodeInt(int num)
+    {
+        int length = ALPHABET.Length;
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) { 
+            sb.Insert(0, ALPHABET[num % length]);
+            num = num / length;
+        }
+        return sb.ToString();
     }
 }
